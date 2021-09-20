412d7215273b5055c68f96f85edd4a2cc2251f73
Rohini <rohini_subramanian@outlook.com>
2021-09-20 20:43:50 +0800

    Example of constructor injection

--- src/main/java/com/example/practice/dependencyInjection/Exam.java
+++ src/main/java/com/example/practice/dependencyInjection/Exam.java
@@ -5,19 +5,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Exam {
     public static void main(String[] args) {
+        /*
         Student student = new Student(); // this is stil creating an object
         student.setStudentName("Jack"); //and hard coding the value by ourselves
         student.displayStudentInfo();
-        //whenever spring creates a student object,
-        //it will inject the value that i have provided for student name
-        //if we want to force spring to create the objects, we must configure the beans
+        whenever spring creates a student object,
+        it will inject the value that i have provided for student name
+        if we want to force spring to create the objects, we must configure the beans
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Student john = context.getBean("student", Student.class);
         john.displayStudentInfo();
-
+         */
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Student jack = context.getBean("Jack",Student.class);
         jack.displayStudentInfo();
+
+        Student john = context.getBean("John",Student.class);
+        john.displayStudentInfo();
 
 
     }
--- src/main/java/com/example/practice/dependencyInjection/Student.java
+++ src/main/java/com/example/practice/dependencyInjection/Student.java
@@ -9,6 +9,15 @@ public class Student {
     Dependency of student
     when I create an object of Student, it should have a name
      */
+
+    public Student(int studentId){
+        this.studentId = studentId;
+    }
+
+    public Student(int studentId, String studentName){
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
     public void displayStudentInfo(){
         System.out.println("Student name is "+studentName);
         System.out.println("Student id is "+studentId);
