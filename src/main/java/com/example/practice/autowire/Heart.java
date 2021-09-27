9638aaa255ef9e86f0b05bbabe2cfa149c395d93
Rohini <rohini_subramanian@outlook.com>
2021-09-28 00:00:21 +0800

    Use autowire="byName" in beans.xml to inject dependency without require <property> tag

---
+++ src/main/java/com/example/practice/autowire/Heart.java
@@ -0,0 +1,8 @@
+package com.example.practice.autowire;
+
+public class Heart {
+    public void pump(){
+        System.out.println("your heart is pumping");
+        System.out.println("Alive");
+    }
+}
---
+++ src/main/java/com/example/practice/autowire/Human.java
@@ -0,0 +1,18 @@
+package com.example.practice.autowire;
+
+public class Human {
+    private Heart heart;
+
+    public void setHeart(Heart heart) {
+        this.heart = heart;
+    }
+
+    public void startPumping(){
+        if(heart!=null){
+            heart.pump();
+        }else{
+            System.out.println("you are dead");
+        }
+
+    }
+}
