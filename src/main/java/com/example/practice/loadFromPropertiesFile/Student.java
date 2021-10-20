570aad3a44abe6a91fead251ddcc92522866d684
Rohini <rohini_subramanian@outlook.com>
2021-10-20 10:42:30 +0800

    Use .properties file to dynamically load values instead of typing static values into beans.xml

---
+++ src/main/java/com/example/practice/loadFromPropertiesFile/Student.java
@@ -0,0 +1,39 @@
+package com.example.practice.loadFromPropertiesFile;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class Student {
+    private String name;
+    private String interestedCourse;
+    private String hobby;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInterestedCourse() {
+        return interestedCourse;
+    }
+
+    public void setInterestedCourse(String interestedCourse) {
+        this.interestedCourse = interestedCourse;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public void displayStudentInfo(){
+        System.out.println("Student name "+ name);
+        System.out.println("Student interested course "+ interestedCourse);
+        System.out.println("Student hobby "+ hobby);
+    }
+}
