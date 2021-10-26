c94ffbf19750cfabd7316896a7f6bc164a6bc1a7
Rohini <rohini_subramanian@outlook.com>
2021-10-26 17:49:38 +0800

    Create java configuration class using @Component and create applicationcontext using AnnotationConfigApplicationContext

--- src/main/java/com/example/practice/springCoreAnnotations/Client.java
+++ src/main/java/com/example/practice/springCoreAnnotations/Client.java
@@ -1,11 +1,12 @@
 package com.example.practice.springCoreAnnotations;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans3.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(CollegeConfig.class);
         System.out.println("beans.xml file loaded");
         College c = context.getBean("collegeBean",College.class);
         c.getName();
