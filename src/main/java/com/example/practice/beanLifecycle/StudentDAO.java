76132e102824a9aed8d21d208af009d99807eaea
Rohini <rohini_subramanian@outlook.com>
2021-10-27 21:45:19 +0800

    Use @PostConstruct annotation to make SpringFramework execute the method once beans are created. This way, you do not need to call the createConnection method in every database method call

--- src/main/java/com/example/practice/beanLifecycle/StudentDAO.java
+++ src/main/java/com/example/practice/beanLifecycle/StudentDAO.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.sql.*;
 
 
@@ -23,9 +24,10 @@ public class StudentDAO {
     Statement stmt = null;
     PreparedStatement pst = null;
 
+    @PostConstruct
     public void createConnection() throws SQLException {
         //get a connection. must provide url, username and password to establish the connection
-        conn = DriverManager.getConnection(url,username,password);
+        conn = DriverManager.getConnection(url, username, password);
 
         //execute a query
         stmt = conn.createStatement();
@@ -37,38 +39,33 @@ public class StudentDAO {
 
     //to fetch all the rows from the table
     public void selectAllRows() throws SQLException {
-        createConnection();
-
         //returns a table
         ResultSet rs = stmt.executeQuery("SELECT * FROM rohinidb.hostelStudentInfo");
 
         //extract each row
-        while(rs.next()){
+        while (rs.next()) {
             int studentId = rs.getInt(1);
             String studentName = rs.getString(2);
             double hostelfee = rs.getDouble(3);
             String food_type = rs.getString(4);
-            System.out.printf("%d %s %.2f %s %n",studentId, studentName, hostelfee, food_type);
+            System.out.printf("%d %s %.2f %s %n", studentId, studentName, hostelfee, food_type);
         }
-
         closeConnection();
     }
 
     public void deleteStudentRecord(int studentId) throws SQLException {
-        createConnection();
-        stmt.executeUpdate("DELETE FROM rohinidb.hostelStudentInfo WHERE student_id="+ studentId);
+        stmt.executeUpdate("DELETE FROM rohinidb.hostelStudentInfo WHERE student_id=" + studentId);
         System.out.println("record deleted");
         closeConnection();
     }
 
-    public void addStudentRecord(int studentId,String studentName, double hostelFee, String foodType) throws SQLException {
-        createConnection();
+    public void addStudentRecord(int studentId, String studentName, double hostelFee, String foodType) throws SQLException {
         String command = "INSERT INTO rohinidb.hostelStudentInfo VALUES (?,?,?,?)";
         pst = conn.prepareStatement(command);
-        pst.setInt(1,studentId);
-        pst.setString(2,studentName);
-        pst.setDouble(3,hostelFee);
-        pst.setString(4,foodType);
+        pst.setInt(1, studentId);
+        pst.setString(2, studentName);
+        pst.setDouble(3, hostelFee);
+        pst.setString(4, foodType);
         pst.executeUpdate();
         closeConnection();
     }
