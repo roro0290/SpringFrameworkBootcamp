1250fc52b9fd507ee60a1f791e830422745ce0f3
Rohini <rohini_subramanian@outlook.com>
2021-11-08 21:52:07 +0800

    Use @Profile("cat") and spring.profiles.active=cat in application.properties to tell spring to inject an instance of cat to Animal

--- src/main/java/com/example/practice/springBootProfile/beans/Cat.java
+++ src/main/java/com/example/practice/springBootProfile/beans/Cat.java
@@ -1,9 +1,11 @@
 package com.example.practice.springBootProfile.beans;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("cat")
 public class Cat implements Animal{
 
     @Override
--- src/main/java/com/example/practice/springBootProfile/beans/Dog.java
+++ src/main/java/com/example/practice/springBootProfile/beans/Dog.java
@@ -1,10 +1,12 @@
 package com.example.practice.springBootProfile.beans;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Profile("dog")
 public class Dog implements Animal{
 
     @Override
