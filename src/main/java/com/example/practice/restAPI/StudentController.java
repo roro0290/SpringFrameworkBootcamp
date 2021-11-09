8088e2f16783d10f8239b70858dcedf8fd8fb6c9
Rohini <rohini_subramanian@outlook.com>
2021-11-09 23:37:37 +0800

    Spring rest api return student list object

--- src/main/java/com/example/practice/restAPI/StudentController.java
+++ src/main/java/com/example/practice/restAPI/StudentController.java
@@ -1,14 +1,29 @@
 package com.example.practice.restAPI;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
 
+    //a method which returns a java bean/object to client
     //http://localhost:8080/student
     @GetMapping("/student")
     public Student getStudent(){
         return new Student("ro","ro");
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("ro","ro"));
+        students.add(new Student("foo","moo"));
+        students.add(new Student("hello","world"));
+        students.add(new Student("jack","jill"));
+        return students;
     }
 }
