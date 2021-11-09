fb9a25aad9408b8bad34d667ffc189358e0553c3
Rohini <rohini_subramanian@outlook.com>
2021-11-10 00:18:04 +0800

    Write a REST API which handles query parameters in url using @QueryParam which will bing query parameter to the method argument

--- src/main/java/com/example/practice/restAPI/StudentController.java
+++ src/main/java/com/example/practice/restAPI/StudentController.java
@@ -3,6 +3,7 @@ package com.example.practice.restAPI;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -28,12 +29,21 @@ public class StudentController {
         return students;
     }
 
-    //http://localhost:8080/ramesh/fadatare
+    //http://localhost:8080/student/ramesh/fadatare
     //write a REST API which will bind the value of these path variables in the method
     //URI template path variable
     //@PathVariable annotation to bind the request URL template path variable to the method variable
     @GetMapping("/student/{firstName}/{lastName}")
     public Student studentPathVariable(@PathVariable("firstName") String first, @PathVariable("lastName") String last){
+        return new Student(first,last);
+    }
+
+    //build a restAPI to handle query parameters
+    //http://localhost:8080/student/query?firstName=Ramesh&lastName=fadatare
+    //each and every rest api endpoint should have a unique uri
+    //Use @RequestParam to bind query parameter to method argument
+    @GetMapping("/student/query")
+    public Student studentQueryParam(@RequestParam("firstName") String first, @RequestParam("lastName") String last){
         return new Student(first,last);
     }
 
